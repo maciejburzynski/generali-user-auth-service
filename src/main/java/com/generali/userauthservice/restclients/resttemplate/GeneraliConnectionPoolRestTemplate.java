f03640250242dcd6fd18efd9105e0217c27cdd97
Maciej Burzyński <maciej.kollaps@gmail.com>
2023-10-12 17:04:45 +0200

    Add connection pool

--- src/main/java/com/generali/userauthservice/restclients/resttemplate/GeneraliConnectionPoolRestTemplate.java
+++ src/main/java/com/generali/userauthservice/restclients/resttemplate/GeneraliConnectionPoolRestTemplate.java
@@ -17,7 +17,6 @@ import java.io.IOException;
 @Slf4j
 public class GeneraliConnectionPoolRestTemplate {
 
-    RestTemplate restTemplate = new RestTemplate();
 
     @PostConstruct
     void executeConnectionPool() throws IOException {
